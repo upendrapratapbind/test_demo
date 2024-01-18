5e803b36cc58cb3e1cd9d1a2bf646b716042d7b8
upendra <upbind027@gmail.com>
2024-01-18 15:19:32 +0530

    commiting java file

---
+++ RemoveSpaceString.java
@@ -0,0 +1,6 @@
+public class RemoveSpaceString{
+public static void main(String args[]){
+StringBuffer strbfr=new StringBuffer("My name is Upendra");
+System.out.println(strbfr.replaceAll("\\s",""));
+}
+}
